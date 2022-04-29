b527bb605f7f86d06c36a632e9eeffde7c163097
Mark Anderson <markcanderson@student.midlandstech.edu>
2022-04-28 22:33:45 -0400

    got working

--- returnLongStringOr00.js
+++ returnLongStringOr00.js
@@ -1,11 +1,28 @@
 // Write a function that takes a string of text and
 // returns the longest word with an even number of
 // letters and if there's no even length return "00"
+const prompt = require("prompt-sync")();
 
-text = "this is a really long sentence";
+let text = prompt("Enter text: ");
 
-function getLongestWord(text) {
-  console.log(text.length);
+function returnStringOr00(text) {
+  let ifEven = false;
+  let newArr = text.split(" ", text.length);
+  let max = newArr[0];
+  for (let i = 1; i < newArr.length; i++) {
+    let length = newArr[i].length;
+    if (length % 2 == 0) {
+      if (max.length < newArr[i].length) {
+        ifEven = true;
+        max = newArr[i];
+        return newArr[i];
+      }
+    }
+  }
+  if (!ifEven) {
+    return "00";
+  }
 }
 
-getLongestWord(text);
+let answer = returnStringOr00(text);
+console.log(answer);
