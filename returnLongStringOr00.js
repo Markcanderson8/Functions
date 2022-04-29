4baff0a58c301996a87929355f7b55fa06d3b53f
Mark Anderson <markcanderson@student.midlandstech.edu>
2022-04-28 21:09:43 -0400

    first commit

---
+++ returnLongStringOr00.js
@@ -0,0 +1,11 @@
+// Write a function that takes a string of text and
+// returns the longest word with an even number of
+// letters and if there's no even length return "00"
+
+text = "this is a really long sentence";
+
+function getLongestWord(text) {
+  console.log(text.length);
+}
+
+getLongestWord(text);
